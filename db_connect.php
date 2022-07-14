0c5214060992783668fd2ef2af261a100bcc9000
onishelltib <85649528+onishelltib@users.noreply.github.com>
2022-07-14 09:18:08 -0400

    Updated db_connect

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('sql104.epizy.com	','epiz_32167485_SMC','epiz_32167485','Coat679he8228')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('lcpbq9az4jklobvq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','u6gaw9bnfc3tiomc','a0yps13oh7tcl76w','vsvldnmhaa4x6nx0')or die("Could not connect to mysql".mysqli_error($con));
